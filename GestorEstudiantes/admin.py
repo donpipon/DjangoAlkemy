4a2fbc477ab047148ffcba33e4f0a6312eaa5b78
donpipon <donpipon@hotmail.com>
2024-03-13 16:41:19 -0300

    clase 11

--- GestorEstudiantes/admin.py
+++ GestorEstudiantes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Estudiante
 
-# Register your models here.
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "apellido", "edad", "nota"]
+    search_fields = ["nombre", "apellido", "edad", "nota"]
+    empty_value_display = "-sin asignar-"
+    
+
+
+admin.site.register(Estudiante, EstudianteAdmin)
--- GestorEstudiantes/models.py
+++ GestorEstudiantes/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Estudiante(models.Model):
     id = models.IntegerField(auto_created=True, null=False, blank=False, primary_key=True)
     nombre = models.CharField(max_length=100, null=False)
-    apellido = models.CharField(max_length=100, null=False)
+    apellido = models.CharField(max_length=100, null=True, blank=True)
     edad = models.IntegerField(max_length=3)
-    nota = models.IntegerField(max_length=2)
+    nota = models.IntegerField(max_length=2, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
